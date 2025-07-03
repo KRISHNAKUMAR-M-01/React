d17499f2677270204cc837a2a47ec9af8e9dc15a
KRISHNAKUMAR-M-01 <kriskanna17@gmail.com>
2025-07-03 19:26:46 +0530

    changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,17 +4,17 @@ export default function Login(){
         <div className="div1">
              <div className='div2'>
                 <label htmlFor="name">Username:</label>
-                <input type="text" placeholder='Enter your name..' id='name'/>
+                <input type="text" placeholder='Enter your name..' id='name' />
                 <br/>
                 <label htmlFor="pw">Password:</label>
                 <input type="password" placeholder='Enter your password..' id='pw'/>
                 <br />
                 <Link to="/ForgetPassword" className='link1'>Forget Password?</Link>
                 <br />
-                <button>Login</button>
+                <button>Sign-in</button>
              </div>
              <div className='div3'>
-                <button><Link to="/Registration" className='div31' >Registration</Link></button>
+                <button><Link to="/Registration" className='div31' >Sign-up</Link></button>
              </div>
         </div>
     );
